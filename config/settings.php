d0e201002fa3d7a926f9d041b22dfe2c3d70d519
eliagbayani <eli@eol.org>
2016-10-06 00:21:48 -0400

    updates

--- config/settings.php
+++ config/settings.php
@@ -3,8 +3,12 @@
 if(!defined('DOWNLOAD_WAIT_TIME')) define('DOWNLOAD_WAIT_TIME', '300000'); //.3 seconds
 define('DOWNLOAD_ATTEMPTS', '2');
 if(!defined('DOWNLOAD_TIMEOUT_SECONDS')) define('DOWNLOAD_TIMEOUT_SECONDS', '30');
+
+/* not used
 define('CACHE_PATH', '/Volumes/MacMini_HD2/sc_cache/');
 define('MEDIAWIKI_MAIN_FOLDER', 'LiteratureEditor');
+*/
+
 define('DOC_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
 /* MySQL credentials */
@@ -13,7 +17,8 @@ define('USERNAME', 'root');
 define('PASSWORD', 'm173');
 define('DBNAME', 'redirect_eoearth');
 
-define('WIKI_DOMAIN', 'editors.eol.localhost');
+// define('WIKI_DOMAIN', 'editors.eol.localhost');
+define('WIKI_DOMAIN', 'editors.eol.org');
 define('MEDIAWIKI_FOLDER', 'eoearth');
 
 ?>
