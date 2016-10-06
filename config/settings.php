70c31bf7383c0546aeb3dd76cd349b32c3543764
eliagbayani <eli@eol.org>
2016-10-06 03:07:49 -0400

    updates

--- config/settings.php
+++ config/settings.php
@@ -1,4 +1,11 @@
 <?php
+/*
+Display all errors except warnings. 
+Did this to not trouble with such msgs:
+Warning: mysqli_connect(): Headers and client library minor version mismatch. Headers:50541 Library:50625 in /var/www/html/eoearth_redirect_sys/controllers/redirect.php on line 71
+Didn't want to solve this at the moment since MySQL is being used by MediaWiki (and probably CKAN) and others in Archive server.
+*/
+error_reporting(E_ALL ^ E_WARNING); //display all errors except warnings in Archive server
 
 if(!defined('DOWNLOAD_WAIT_TIME')) define('DOWNLOAD_WAIT_TIME', '300000'); //.3 seconds
 define('DOWNLOAD_ATTEMPTS', '2');
